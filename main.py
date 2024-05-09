81a8d68fbffa5b215d18a9a4dd6438b09524f22f
mayday <maydaythecoder@gmail.com>
2024-05-08 21:49:20 -0400

    SoftMax activation

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-
 import numpy as np
 import nnfs
 from nnfs.datasets import spiral_data
 
 nnfs.init()
 
-X, y = spiral_data(100, 3)
-
 
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons):
         self.output = None
-        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.weights = 0.1 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-        
+
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
@@ -26,10 +23,28 @@ class Activation_ReLU:
         self.output = np.maximum(0, inputs)
 
 
-layer1 = Layer_Dense(2, 5)
+class Activation_Softmax:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
 activation1 = Activation_ReLU()
 
-layer1.forward(X)
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
 
-activation1.forward(layer1.output)
-print(activation1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
