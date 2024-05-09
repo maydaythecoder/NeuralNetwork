0dc5e9dfd17d83b5e51b8e489ef5f06503b0b9c4
mayday <maydaythecoder@gmail.com>
2024-05-08 21:30:33 -0400

    added weight and input

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-inputs = [1.2, 5.1, 2.1]
-weights = [3.1, 2.1, 8.7]
-bias = 3
+inputs = [1.0, 2.0, 3.0, 2.5]
+weights = [0.2, 0.8, -0.5, 1.0]
+bias = 2.0
 
-output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + bias
+output = inputs[0]*weights[0] + inputs[1]*weights[1] + inputs[2]*weights[2] + inputs[3]*weights[3] + bias
 print(output)
+
