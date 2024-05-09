672e3a48ee4a617c8c0c194c0f0696f27c51eefb
mayday <maydaythecoder@gmail.com>
2024-05-08 21:52:17 -0400

    Applying Categorical Cross Entropy loss to the nnfs framework

--- main.py
+++ main.py
@@ -1,13 +1,78 @@
-import math
+import numpy as np
+import nnfs
+from nnfs.datasets import spiral_data
 
-softmax_output = [0.7, 0.1, 0.2]
-target_output = [1, 0, 0]
+nnfs.init()
 
-loss = -(math.log(softmax_output[0]) * target_output[0] +
-         math.log(softmax_output[1]) * target_output[1] +
-         math.log(softmax_output[2]) * target_output[2])
 
-print(loss)
+class Layer_Dense:
+    def __init__(self, n_inputs, n_neurons):
+        self.output = None
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
 
-print(-math.log(0.7))
-print(-math.log(0.5))
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+class Activation_ReLU:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+
+class Activation_Softmax:
+    def __init__(self):
+        self.output = None
+
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+        self.output = probabilities
+
+
+class Loss:
+    def calculate(self, output, y):
+        sample_losses = self.forward(output, y)
+        data_loss = np.mean(sample_losses)
+        return data_loss
+
+
+class Loss_CategoricalCrossentropy(Loss):
+    @staticmethod
+    def forward(y_pred, y_true):
+        samples = len(y_pred)
+        y_pred_clipped = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
+        if len(y_true.shape) == 1:
+            correct_confidences = y_pred_clipped[range(samples), y_true]
+
+        elif len(y_true.shape) == 2:
+            correct_confidences = np.sum(y_pred_clipped * y_true, axis=1)
+
+        negative_log_likelihoods = -np.log(correct_confidences)
+        return negative_log_likelihoods
+
+
+X, y = spiral_data(samples=100, classes=3)
+
+dense1 = Layer_Dense(2, 3)
+activation1 = Activation_ReLU()
+
+dense2 = Layer_Dense(3, 3)
+activation2 = Activation_Softmax()
+
+dense1.forward(X)
+activation1.forward(dense1.output)
+
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
+
+print(activation2.output[:5])
+
+loss_function = Loss_CategoricalCrossentropy()
+loss = loss_function.calculate(activation2.output, y)
+
+print("Loss:", loss)
