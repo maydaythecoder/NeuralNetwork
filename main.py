298fea41b149cd0028efcdba20254ed40a785d77
mayday <maydaythecoder@gmail.com>
2024-05-08 21:44:25 -0400

    added layers

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
-import numpy as np
+import numpy as np 
 
-inputs = [1.0, 2.0, 3.0, 2.5]
-weights = [[0.2, 0.8, -0.5, 1.0],
-           [0.5, -0.91, 0.26, -0.5],
-           [-0.26, -0.27, 0.17, 0.87]]
+np.random.seed(0)
 
-biases = [2.0, 3.0, 0.5]
+X = [[1, 2, 3, 2.5],
+     [2.0, 5.0, -1.0, 2.0],
+     [-1.5, 2.7, 3.3, -0.8]]
 
-output = np.dot(weights, inputs) + biases
-print(output)
+
+class layerDense:
+    def __init__(self, n_inputs, n_neurons):
+        self.output = None
+        self.weights = 0.10 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1, n_neurons))
+        
+    def forward(self, inputs):
+        self.output = np.dot(inputs, self.weights) + self.biases
+
+
+layer1 = layerDense(4, 5)
+layer2 = layerDense(5, 2)
+
+layer1.forward(X)
+layer2.forward(layer1.output)
+print(layer2.output)
